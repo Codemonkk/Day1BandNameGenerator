0bdec53ce03d8324e188d210758f76c38461eed4
Utkarsh Kashyap <utkarshkashyap@Utkarshs-MacBook-Air.local>
2023-01-02 13:53:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+c = input("What's the name of city you were born in? ")
+p = input("What's the name of your pet? ")
+
+print("your band name could be " + c + " " + p)
+
